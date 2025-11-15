f0cca1fa94f33b66bcdacb74def055ba5323466d
jzlnikola <jzlnikola@gmail.com>
2025-11-15 17:07:43 +0100

    navigation completed

--- app/src/main/java/elfak/mosis/medspot/activities/MainActivity.kt
+++ app/src/main/java/elfak/mosis/medspot/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package elfak.mosis.medspot.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,37 +11,81 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 import elfak.mosis.medspot.ui.theme.MedSpotTheme
+import android.content.Intent
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.navigation.findNavController
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import elfak.mosis.medspot.R
 
-class MainActivity : ComponentActivity() {
+
+
+
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var auth : FirebaseAuth
+    private lateinit var navController: NavController
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MedSpotTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        navController = findNavController(R.id.nav_host_fragment_content_loginandsignup)
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> {
+                navController.popBackStack()
+                navController.navigate(R.id.HomeFragment)
+            }
+            R.id.nav_map -> {
+                navController.popBackStack()
+                navController.navigate(R.id.MapFragment)
+            }
+            R.id.nav_logout -> {
+                auth.signOut()
+                val i: Intent = Intent(this, LoginAndSignUpActivity::class.java)
+                startActivity(i)
+                finish()
             }
         }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MedSpotTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/elfak/mosis/medspot/screens/HomeFragment.kt
@@ -0,0 +1,61 @@
+package elfak.mosis.medspot.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import elfak.mosis.medspot.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import elfak.mosis.medspot.models.data.User
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class HomeFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        val welcomeText: TextView = requireView().findViewById(R.id.welcome_textView)
+        welcomeText.text =" Welcome \n" +  auth.currentUser?.email?.substringBefore("@")
+
+        val database = Firebase.database("https://nijo-medspot-id-default-rtdb.europe-west1.firebasedatabase.app/")
+            .reference.child("users").child(auth.currentUser?.uid.toString())
+
+        val mapButton = view.findViewById<Button>(R.id.button)
+        mapButton.setOnClickListener {
+            findNavController().navigate(R.id.action_HomeFragment_to_MapFragment)
+        }
+    }
+}
